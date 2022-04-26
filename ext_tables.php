6efbc57cb05001b89ce0dcd5fcb17921956dd400
vermitte <vermitte@s78.server.hostpoint.internal>
2022-04-26 10:14:56 +0200

    extname changes

--- ext_tables.php
+++ ext_tables.php
@@ -8,6 +8,6 @@ defined('TYPO3') || die();
     \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr('tx_indiznews_domain_model_category', 'EXT:indiz_news/Resources/Private/Language/locallang_csh_tx_indiznews_domain_model_category.xlf');
     \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_indiznews_domain_model_category');
 
-    $GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist']['indiznews_news'] = 'pi_flexform';
-     \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue('indiznews_news', 'FILE:EXT:indiz_news/Configuration/Flexform/flexform.xml');
+    $GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist']['musikschule_news'] = 'pi_flexform';
+     \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue('musikschule_news', 'FILE:EXT:musikschule_news/Configuration/Flexform/flexform.xml');
 })();
